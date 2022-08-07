52aadf2ece9bc37ce22eac523d909d349bd7cc9c
unknown <buragohain4@gmail.com>
2022-08-07 10:06:49 +0530

    Adding Reverse Program

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,  remainder, reverse = 0;
+
+            Console.WriteLine("Enter an Number to be reversed \n");
+
+            num = int.Parse(Console.ReadLine());
+
+           
+            while (num > 0)
+            {
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num /= 10;
+            }
+
+            Console.WriteLine("The reverse of the number is = {0}", reverse);
+            Console.Read();
+        }
+    }
+}
