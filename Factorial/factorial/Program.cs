bc3cb99394907e224f23348c70191fa62d8135d2
unknown <buragohain4@gmail.com>
2022-08-07 10:00:49 +0530

    Implemented Factorial program

---
+++ Factorial/factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Program.factorial(y);
+        }
+        public static void factorial(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine($"{i} is the factor of {num}");
+                }
+            }
+
+        }
+    }
+}
