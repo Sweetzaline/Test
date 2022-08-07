736e27b2f5a19bb3bff9c25f2268a1d2cb17dd5a
unknown <buragohain4@gmail.com>
2022-08-07 10:05:08 +0530

    Implemented Factors Porgram

---
+++ Factors/Fact/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Fact
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+            Console.WriteLine("Enter a number to find the factorial: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            Factorial fact = new Factorial();
+            answer = fact.methodForFactorial(value);
+            Console.WriteLine("Factorial of your input is : {0}", answer);
+            Console.ReadLine();
+        }
+
+        public int methodForFactorial(int n)
+        {
+            int answer = 1;
+
+            while (n != 1)
+            {
+                answer = answer * n;
+                n = n - 1;
+            }
+            return answer;
+        }
+    }
+}
